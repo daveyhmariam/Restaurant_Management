46e71f20ebe6216a80bb216890a5cff5a99b4a5d
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-06-02 00:58:30 +0300

    Revert "For DB Storage:"
    This reverts commit c71381ca4b62e6945fff4dac4be8d174a4d13095.

--- models/engine/fs.py
+++ models/engine/fs.py
@@ -3,12 +3,6 @@
 """
 from models.base_model import BaseModel
 import json
-from models.users import User
-from models.orders import Order
-from models.order_item import OrderItem
-from models.menu_items import MenuItem
-from models.recipes import Recipe
-from models.inventory_items import InventoryItem
 
 
 class FileStorage:
@@ -18,27 +12,18 @@ class FileStorage:
     __file = "objects.json"
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """
         return dictionary that holds all objects or objects of
         cls if cls is provided
         """
-        if cls:
-            obj = {}
-            all_obj = self.__objects
-            for key in list(all_obj):
-                key_split = key.split(".")
-                cls_name = key_split[0]
-                if cls_name == cls.__name__:
-                    obj.update({key: all_obj[key]})
-            return obj
-        return self.__objects.copy()
+        return type(self).__objects.copy()
 
     def delete(self, obj=None):
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            if key in self.__objects.keys():
-                del self.__objects[key]
+            if key in type(self).__objects.keys():
+                del type(self).__objects[key]
 
     def new(self, obj):
         """
@@ -48,7 +33,7 @@ class FileStorage:
             obj (instance): new object to add
         """
         if obj:
-            self.__objects["{}.{}".format(type(obj).__name__,
+            type(self).__objects["{}.{}".format(type(obj).__name__,
                                  obj.id)] = obj
 
     def save(self):
@@ -57,9 +42,9 @@ class FileStorage:
 
         """
         try:
-            with open(self.__file, "w") as file:
+            with open(type(self).__file, "w") as file:
                 temp = {}
-                temp.update(self.__objects)
+                temp.update(type(self).__objects)
                 for key, value in temp.items():
                     temp[key] = value.to_dict()
                 json.dump(temp, file, indent=4)
@@ -68,21 +53,14 @@ class FileStorage:
 
     def reload(self):
 
-        classes = {"BaseModel": BaseModel,
-                    'User': User,
-                    'Order': Order,
-                    'OrderItem': OrderItem,
-                    'MenuItem': MenuItem,
-                    'Recipe': Recipe,
-                    'InventoryItem': InventoryItem
-        }
+        classes = {"BaseModel": BaseModel}
 
         try:
             temp = {}
-            with open(self.__file, "r") as file:
+            with open(type(self).__file, "r") as file:
                 temp = json.load(file)
             for key, value in temp.items():
-                self.__objects[key] = classes[value["__class__"]](**value)
+                type(self).__objects[key] = classes[value["__class__"]](**value)
         except (FileNotFoundError, json.JSONDecodeError):
             pass
 
