65c35638053f25a2ef3200e835885fa6a071e922
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-06-09 22:58:09 +0300

    database setup

---
+++ en.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+export PYTHONPATH=/home/dave/alx_2/Restaurant_Management
+
+
+export MYSQL_USER=hbnb_dev
+export MYSQL_PWD=daveyhmdave
+export MYSQL_DB=dev_db
+export MYSQL_HOST=localhost
+
+
+export TYPE_STORAGE=db
